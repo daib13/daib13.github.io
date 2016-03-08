21e5df886ca815e7d7fc356e548cdb881658d260
daib13 <daib09physics@gmail.com>
2016-03-08 23:24:31 +0800

    Update yin.js

--- assets/js/yin.js
+++ assets/js/yin.js
@@ -2,7 +2,7 @@ var yinState = 0;
 function showYinFunc() {
 	if(yinState == 0) {
 		$('#yinImg').attr('src', '/images/yinzhangpost.jpg');
-		$('#yinDiscription').html('Scan the QR code to follow her WeChat subscription. Believe me, she\'s a great beauty.);
+		$('#yinDiscription').html('Scan the QR code to follow her WeChat subscription. Believe me, she\'s a great beauty.');
 		yinState = 1;
 	}
 	else {
