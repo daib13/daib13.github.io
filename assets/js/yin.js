9159135516fa57ec5b2ffaa4884eb2c0bf79147d
Bin Dai <v-bindai@mslpa.corp.microsoft.com>
2016-03-08 23:14:04 +0800

    modify the id card design add a js for Yin's link

---
+++ assets/js/yin.js
@@ -0,0 +1,13 @@
+var yinState = 0;
+function showYinFunc() {
+	if(yinState == 0) {
+		$('#yinImg').attr('src', '/images/yinzhangpost.jpg');
+		$('#yinDiscription').html('Scan the QR code with your to follow her WeChat subscription. She\'s a great beauty you\'ll never know enough about');
+		yinState = 1;
+	}
+	else {
+		$('#yinImg').attr('src', '/images/yinzhang.jpg');
+		$('#yinDiscription').html('Ph.D. candidate in the School of Architecture, Tsinghua Unviersity.');
+		yinState = 0;
+	}
+}
